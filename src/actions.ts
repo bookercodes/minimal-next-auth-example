c0bf21501878fcb5b5f307d2b2d944179e19509a
Alex Booker <booker@booker.codes>
2024-10-05 12:54:47 +0100

    impl sign-up

--- src/actions.ts
+++ src/actions.ts
@@ -6,10 +6,16 @@ import bcrypt from "bcrypt"
 import { createSession } from "./auth"
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
+import { error } from "console"
 
 const SignUpSchema = z.object({
-  username: z.string().min(3, "Username must be at least 3 characters"),
-  password: z.string().min(8, "Password must be at least 8 characters")
+  username: z.string(),
+  password: z.string()
+})
+
+const SignInSchema = z.object({
+  username: z.string(),
+  password: z.string()
 })
 
 export type ActionState = {
@@ -18,6 +24,56 @@ export type ActionState = {
     password?: string[]
   }
   message?: string
+}
+
+export async function signIn(
+  prevState: ActionState,
+  formData: FormData
+): Promise<ActionState> {
+  const validatedFields = SignInSchema.safeParse({
+    username: formData.get("username"),
+    password: formData.get("password")
+  })
+
+  const errorMessage = { message: "Invalid login credentials." }
+
+  // If any form fields are invalid, return early
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors
+    }
+  }
+
+  const stmt = db.prepare("SELECT * FROM users WHERE name = ?")
+  const user: any = stmt.get(validatedFields.data.username)
+
+  if (!user) {
+    return errorMessage
+  }
+
+  const passwordMatch = await bcrypt.compare(
+    validatedFields.data.password,
+    user.password
+  )
+
+  if (!passwordMatch) {
+    return errorMessage
+  }
+
+  const sesh = createSession(user.user_id)
+  console.log("sesh", sesh)
+  const sesh_id = sesh?.id
+
+  cookies().set("session", `${sesh_id}`, {
+    httpOnly: true,
+    secure: true,
+    expires: sesh?.endTime,
+    sameSite: "lax",
+    path: "/"
+  })
+
+  redirect("/dashboard")
+  return { message: "success" }
 }
 
 export async function signUp(
